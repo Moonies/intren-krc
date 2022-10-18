404fe148ca76f16631e104fbe52944fbf3b28aff
sarun.p <bungtwoza@gmail.com>
2022-10-18 17:46:45 +0900

    remove not used for build

--- src/components/Header/components/TitleHeader/TitleHeader.style.ts
+++ src/components/Header/components/TitleHeader/TitleHeader.style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { colors, fontSizes } from 'theme'
+import { colors } from 'theme'
 const HeaderTitle = styled.div`
   background-color: ${colors.primary};
   height: 56px;
